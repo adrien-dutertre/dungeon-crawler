ed8c413b4c6d88ab1d461625054e5fcc61df40e4
Poutou <dutertre.adrien@pm.me>
2026-07-15 18:13:39 +0200

    Filtre 3 items dans le shop. ToFix: éviter qu'il y ait 2 fois le même item.

--- src/app/features/shop/shop-service.ts
+++ src/app/features/shop/shop-service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Items } from '../../shared/services/items';
 import { Item } from '../inventory/models/item';
 import { Hero } from '../hero-sheet/services/hero';
@@ -8,9 +8,26 @@ import { GameLogic } from '../../shared/services/game-logic';
   providedIn: 'root',
 })
 export class ShopService {
-  list = inject(Items).list;
+  items = inject(Items).list;
   hero = inject(Hero);
   logic = inject(GameLogic);
+
+  shoppingList = signal<Item[]>(this.shopping());
+
+  // Affichage de la boutique
+  shopping(): Item[] {
+    const SHOPPING_ITEMS: number = 3;
+    const list: Item[] = [];
+    const listOrdered: Item[] = this.items.sort(
+      (a, b) => b.chance_of_appearing - a.chance_of_appearing,
+    );
+    while (list.length < SHOPPING_ITEMS) {
+      let random: number = Math.floor(Math.random() * this.items.length);
+
+      list.push(listOrdered[random]);
+    }
+    return list;
+  }
 
   // Shopping interactions
   buy(item: Item): void {
